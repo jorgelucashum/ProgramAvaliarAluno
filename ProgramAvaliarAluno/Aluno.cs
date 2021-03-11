3baa00d90e64c3a5a59665f02ad4b5480399a2a1
jorgeucas lucas <jorge_dofus1@hotmail.com>
2021-03-11 17:25:38 -0300

    Add project files.

---
+++ ProgramAvaliarAluno/Aluno.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramAvaliarAluno
+{
+    class Aluno
+    {
+        public string Nome;
+        public double N1, N2, N3;
+
+        public double NotaFinal()
+        {
+            double notaF = N1 + N2 + N3;
+            return notaF;   
+        }
+
+        public bool Aprovacao()
+        {
+            if (NotaFinal() >= 60)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        public double NotaRestante()
+        {
+            if (Aprovacao())
+            {
+                return 0;
+            } else
+            {
+                return 60 - NotaFinal();
+            }
+           
+
+        }
+    }
+}
---
+++ ProgramAvaliarAluno/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramAvaliarAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.WriteLine("Digite seu nome: ");
+            aluno.Nome = Console.ReadLine();
+
+            do
+            {
+                Console.WriteLine("Digite a nota do primeiro trimestre (max: 30) ");
+                aluno.N1 = double.Parse(Console.ReadLine());
+                Console.WriteLine();
+            } while (aluno.N1 > 30);
+
+            do
+            {
+                Console.WriteLine("Digite a nota do segundo trimestre (max: 35) ");
+                aluno.N2 = double.Parse(Console.ReadLine());
+                Console.WriteLine();
+            } while (aluno.N2 > 35);
+
+            do
+            {
+                Console.WriteLine("Digite a nota do terceiro trimestre: (max: 35)");
+                aluno.N3 = double.Parse(Console.ReadLine());
+                Console.WriteLine();
+            } while (aluno.N3 > 35);
+
+
+            Console.WriteLine(aluno.Nome);
+            if (aluno.Aprovacao())
+            {
+                Console.WriteLine("Nota final: " + aluno.NotaFinal().ToString("F2") + "\nAPROVADO");
+            } else
+            {
+                Console.WriteLine("Nota Final: " + aluno.NotaFinal().ToString("F2") + "\nREPROVADO \nFaltaram: " + aluno.NotaRestante().ToString("F2"));
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
